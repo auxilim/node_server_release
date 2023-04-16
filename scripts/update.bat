c5c3b6fa6fc7cbe3a4a77bc7157d7cf280ad8018
pmouget <109213694+pmouget@users.noreply.github.com>
2023-04-16 17:33:52 +0200

    adding lot of files

---
+++ scripts/update.bat
@@ -0,0 +1,7 @@
+Rem This batch will delete the old AuxilimNode exe and move the new release to its place
+Rem This batch need to be started while the AuxilimNode does not work
+echo off
+powershell -Command "& {sleep 5}"
+del ..\\AuxilimNode.exe
+move ..\newRelease.exe ..\AuxilimNode.exe
+exit
