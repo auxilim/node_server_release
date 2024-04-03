4ffb87394d77272c20258bc5c0a22de2645e666a
pmouget <109213694+pmouget@users.noreply.github.com>
2024-04-03 19:26:28 +0200

    Update update.bat

--- scripts/update.bat
+++ scripts/update.bat
@@ -1,7 +1,8 @@
 Rem This batch will delete the old AuxilimNode exe and move the new release to its place
 Rem This batch need to be started while the AuxilimNode does not work
 echo off
-powershell -Command "& {sleep 5}"
+Rem powershell -Command "& {sleep 5}"
+timeout 5
 del AuxilimNode.exe
 move newRelease.exe AuxilimNode.exe
 exit
