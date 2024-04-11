f08cf5fef6f3d80eafb1fef3d0fa8436d218389c
paul_m <mygoa.official@gmail.com>
2024-04-11 21:29:19 +0200

    add inno setup config + pycharm config run

--- scripts/update.bat
+++
@@ -1,7 +0,0 @@
-Rem This batch will delete the old AuxilimNode exe and move the new release to its place
-Rem This batch need to be started while the AuxilimNode does not work
-powershell -Command "& {sleep 5}"
-Rem End of timeout
-del AuxilimNode.exe
-move newRelease.exe AuxilimNode.exe
-exit
